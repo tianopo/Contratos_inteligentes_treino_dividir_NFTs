336e71dab4a2616907fd716aa817b9be259752f6
matheus <matheuslink18@hotmail.com>
2023-06-09 19:01:35 -0300

    feat: adição hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v6',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false // defaults to false
+  },
   defaultNetwork: "mumbai",
   networks: {
     hardhat: {
