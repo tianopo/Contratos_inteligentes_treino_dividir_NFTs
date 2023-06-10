24476b19ebd61d998c5595960fe89fb1fafc2be8
matheus <matheuslink18@hotmail.com>
2023-06-10 11:35:46 -0300

    feat: contrato corrigido para deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,22 +5,21 @@ import "@nomicfoundation/hardhat-ethers"
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unstorageTime = currentTimestampInSeconds + 60;
-  
+  console.log("approved list:")
   const storageAmount = ethers.parseEther("0.001");
-  console.log("parse")
+  console.log("parse || price")
 
   const Storage = await hre.ethers.getContractFactory("Storage");
   console.log("contract")
   const storage = await Storage.deploy();
   console.log("deployment")
-  await storage.deployed();
+  await storage.waitForDeployment();
   console.log("deployed")
 
   console.log(
     `storage with ${ethers.formatEther(
       storageAmount
-    )}ETH and unstorage timestamp ${unstorageTime} deployed to ${storage.target},
-      address contract: ${storage.address}`
+    )}ETH and unstorage timestamp ${unstorageTime} deployed to ${storage.target}` //pega o endereço do contrato
   );
 }
 
